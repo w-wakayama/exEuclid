40eaa5bb471a41b6fa5ac11ae8d3c9df00cb4113
w-wakayama <tsumamist715@gmail.com>
2018-06-06 11:11:21 +0900

    拡張ユークリッド互除法っぽいのができた。

--- exEuclid.js
+++ exEuclid.js
@@ -1,9 +1,43 @@
-var m = 121;
-var n = 1100;
+var m = 14;
+var n = 35;
 
-var a = gcd(m,n);
-console.log(a); //11
+var ans = exEuclid(m,n);
+console.log(ans);
 
+function exEuclid(m,n){
+	if(m<n){
+		var r = m;
+		var m = n;
+		var n = r;
+	}
+
+	var l = gcd(m,n);
+
+	var r0 = m,
+			r1 = n,
+			x0 = 1,
+			x1 = 0,
+			y0 = 0,
+			y1 = 1;
+	while(r1>0){
+		q1 = r0 / r1;　//r0 / r1 = q1...r2
+		r2 = r0 % r1;
+
+	  x2 = x0 - q1*x1; //r2 = r0 - r1*q1
+	  y2 = y0 - q1*y1;
+
+	  r0 = r1;
+		r1 = r2;
+	  x0 = x1;
+		x1 = x2;
+	  y0 = y1;
+		y1 = y2;
+	}
+	return {a: m, b: n, x: x0, y: y0, gcd: l}
+};
+
+
+//最大公約数を求める
 function gcd(m,n){
 	var r;
 
