fb6b23f70bc6a72029f4652912b54c53daf7a6b4
w-wakayama <tsumamist715@gmail.com>
2018-06-09 12:49:07 +0900

    コメントアウト追加

--- exEuclid.js
+++ exEuclid.js
@@ -1,3 +1,4 @@
+//m*x + n*y = gcd(m,n)
 var m = 14;
 var n = 35;
 
@@ -5,25 +6,28 @@ var ans = exEuclid(m,n);
 console.log(ans);
 
 function exEuclid(m,n){
+	//入れ替え
 	if(m<n){
 		var r = m;
 		var m = n;
 		var n = r;
 	}
 
+	//最大公約数を求める
 	var l = gcd(m,n);
 
 	var r0 = m,
 			r1 = n,
-			x0 = 1,
+			x0 = 1, //m = (m*1)+(n*0)
 			x1 = 0,
-			y0 = 0,
+			y0 = 0, //n = (m*0)+(n*1)
 			y1 = 1;
+
 	while(r1>0){
-		q1 = r0 / r1;　//r0 / r1 = q1...r2
+		q1 = r0 / r1;　//r0/r1 = q1...r2
 		r2 = r0 % r1;
 
-	  x2 = x0 - q1*x1; //r2 = r0 - r1*q1
+	  x2 = x0 - q1*x1;
 	  y2 = y0 - q1*y1;
 
 	  r0 = r1;
@@ -37,7 +41,6 @@ function exEuclid(m,n){
 };
 
 
-//最大公約数を求める
 function gcd(m,n){
 	var r;
 
